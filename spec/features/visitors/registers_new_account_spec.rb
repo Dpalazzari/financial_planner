c3320993e3d32eb23e17247993b7a2a0925b0617
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-21 16:58:23 -0600

    Add failed registration test

--- spec/features/visitors/registers_new_account_spec.rb
+++ spec/features/visitors/registers_new_account_spec.rb
@@ -16,4 +16,23 @@ RSpec.feature 'Visitor Registration' do
       expect(page).to have_content("Successfully Registered, Drew!")
     end
   end
+
+  context 'Unsuccessful Login' do
+    it "redirects back to the register user form" do
+      visit new_user_path
+      within('form') do
+        fill_in "user[name]", with: 'Drew'
+        fill_in "user[email]", with: 'drew@email.com'
+        fill_in "user[budget]", with: '1000'
+        fill_in "user[password]", with: 'password'
+        click_on 'Register'
+      end
+
+      within('form') do
+        expect(page).to have_content("can't be blank")
+      end
+
+      expect(page).to have_content("Please make sure all fields are correctly filled out.")
+    end
+  end
 end
