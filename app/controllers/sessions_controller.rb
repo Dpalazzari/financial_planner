ffd4029183e581d2b1bd0908205c412ae7881699
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-21 19:03:45 -0600

    Add logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    reset_session
+    flash[:success] = "Successfully Logged Out."
+    redirect_to login_path
+  end
+
 end
