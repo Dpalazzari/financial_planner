e7316b8dd5647176b84f2e2af0166c3eb4254078
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-21 19:03:20 -0600

    Add test for logging out

--- spec/features/users/log_in_spec.rb
+++ spec/features/users/log_in_spec.rb
@@ -1,25 +1,52 @@
 require 'rails_helper'
 
 RSpec.feature 'User' do
+
+  before(:each) do
+    @user = create(:user)
+  end
   context 'successfully logs in' do
     it 'directs the user to the dashboard page' do
-      user = create(:user)
-
       visit login_path
 
       expect(page).to have_link("Login")
       expect(page).to_not have_link("Logout")
 
       within("form") do
-        fill_in "email", with: user.email
-        fill_in "password", with: user.password
+        fill_in "email", with: @user.email
+        fill_in "password", with: @user.password
         click_on "Login"
       end
 
       expect(current_path).to eq(dashboard_path)
-      expect(page).to have_content("Logged in as #{user.name}.")
+      expect(page).to have_content("Logged in as #{@user.name}.")
       expect(page).to_not have_link("Login")
       expect(page).to have_content("Logout")
     end
-   end
+  end
+
+  context 'unsuccessfully logs in' do
+    it "redirects to the login path" do
+      visit login_path
+
+      within('form') do
+        fill_in "email", with: @user.email
+        fill_in "password", with: 'big babooshkas'
+        click_on "Login"
+      end
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Login Failed.")
+    end
+  end
+
+  context 'logs out' do
+    it 'logs the user out' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit dashboard_path
+      click_on 'Logout'
+      expect(page).to have_content("Successfully Logged Out.")
+      expect(current_path).to eq(login_path)
+    end
+  end
 end
