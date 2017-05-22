eaced3f5521dd543ca299066d48e20e70bd6dcd5
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-21 18:53:11 -0600

    Add user authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   get '/dashboard' => 'users#show'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
 end
