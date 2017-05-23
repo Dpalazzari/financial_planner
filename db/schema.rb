f035880532ed5cebe9de01628739d36c85662ba1
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-22 19:28:37 -0600

    Migrate expenses table into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521215515) do
+ActiveRecord::Schema.define(version: 20170523012322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.float    "cost"
+    t.datetime "due_date"
+    t.string   "details"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170521215515) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "expenses", "users"
 end
