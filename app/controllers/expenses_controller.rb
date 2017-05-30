d415f521dc960eafe5a580286e69aabea65022cc
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-30 11:30:02 -0600

    Pass the add expense feature test

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,21 @@
+class ExpensesController < ApplicationController
+
+  def create
+    @user    = User.find(params[:user_id])
+    @expense = @user.expenses.new(expense_params)
+    if @expense.save
+      flash[:info]  = "Successfully created an expense."
+      redirect_to dashboard_path
+    else
+      flash[:failure] = "Make sure all required fields are filled in."
+      redirect_to dashboard_path
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:cost, :details, :user_id)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'welcome#index'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] do
+    resources :expenses, only: [:create]
+  end
   get '/dashboard' => 'users#show'
 
   get '/login' => 'sessions#new'
