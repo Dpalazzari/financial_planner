27a2958408dee44a0ba553db47ff739d2ebf65af
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-31 11:20:35 -0600

    Write test for deleting an expense

---
+++ spec/features/users/deletes_an_expense_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature 'User with expenses' do
+  scenario 'deletes an expense successfully' do
+    user    = create(:user)
+    expense = create(:expense)
+    user.expenses << expense
+
+    page.set_rack_session(user_id: user.id)
+
+    expect(user.expenses.count).to eq(1)
+    visit dashboard_path
+    save_and_open_page
+    
+    click_on "Remove"
+
+    expect(current_path).to eq(dashboard_path)
+    expect(user.expenses.count).to eq(0)
+  end
+end
