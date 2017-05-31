e7645b9c791efac4b1c0d1b23813738dc1b06360
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-30 21:32:03 -0600

    Add due_date to expense model tests again

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Expense, type: :model do
   context 'validations' do
     it { is_expected.to validate_presence_of :cost }
+    it { is_expected.to validate_presence_of :due_date }    
     it { is_expected.to validate_presence_of :details }
   end
 
