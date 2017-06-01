1067683eafd661c14bb35bb60640c265ef8fec8f
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-31 20:31:28 -0600

    Add a quick fix to the user model for when only the budget needs to be made.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :name, :email, :budget, :password, presence: true
+  validates :name, :email, :budget, presence: true
+  validates :password, presence: true, :on => :create
   validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, :on => :create
   validates :email, uniqueness: true
 
   has_many :expenses
